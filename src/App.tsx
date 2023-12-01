4f96578beea94f1d6d5bd7e049beeb3296dda376
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-01 19:45:14 +0530

    Adding the basic template

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div id='main-div'>
+      <h1>Increment/Decrement counter</h1>
+      <p>using React and Redux</p>
+      <div>
+        <span>-</span>
+        <span>0</span>
+        <span>+</span>
+      </div>
+    </div>
+  )
+}
+
+export default App
